59e82397791c949ead9e181470af42d946da6eae
Byeori Kim <bk.byeori.kim@gmail.com>
2024-09-17 01:52:03 -0700

    initialized react hooks

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import { Loader, Card, FormField } from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = usLState(false);
+  const [allPosts, setAllPosts] = uAeState(null);
+
   return (
-    <div>Home</div>
+    <section className="max-w-7x1 mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text=[32px]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
   )
 }
 
